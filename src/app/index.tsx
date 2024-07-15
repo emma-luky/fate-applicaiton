bb9f48a2dcb1a6b99c6c995c3918899ef2682b82
emma-luky <emma07315@gmail.com>
2024-07-15 11:14:10 -0600

    firebase updated

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,14 @@
 import { Stack } from 'expo-router';
 import { ScrollView } from 'tamagui';
-import { NewUserForm } from '../components/NewUserForm';
+
+import { PostListView } from '../components/PostListView';
 
 export default function App() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
       <ScrollView flex={1}>
-        <NewUserForm></NewUserForm>
+        <PostListView></PostListView>
       </ScrollView>
     </>
   );
