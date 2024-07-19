3f414cd4e6cf17d2c878cb3852d845d4a9bf97fb
emma-luky <emma07315@gmail.com>
2024-07-18 21:32:42 -0600

    created posts and saved tabs on profile page

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router';
-import { H1, ScrollView } from 'tamagui';
+import { H1, ScrollView, Tabs, SizableText, H5, YStack, Avatar } from 'tamagui';
 
 import { PostListView } from '../components/PostListView';
 import { NavBar } from '../components/NavBar';
@@ -15,8 +15,30 @@ export default function App() {
         }}
       />
       <ScrollView flex={5}>
-        <H1>User Name</H1>
-        <PostListView />
+        <H1 alignSelf='center' marginBottom={15}>User Name</H1>
+        <Avatar circular size="$6" alignSelf='center'>
+          <Avatar.Image src="http://picsum.photos/200/300"/>
+          <Avatar.Fallback bc="red"/>
+        </Avatar>
+        <Tabs defaultValue="Posts" marginTop={25}>
+          <YStack flexDirection='column' alignItems='center'>
+            <Tabs.List>
+              <Tabs.Tab value="Posts" width={195}>
+                <SizableText>Posts</SizableText>
+              </Tabs.Tab>
+              <Tabs.Tab value="Saved" width={195}>
+                <SizableText>Saved</SizableText>
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Content value="Posts">
+              <H5>Posts</H5>
+            </Tabs.Content>
+            <Tabs.Content value="Saved">
+              <H5>Saved</H5>
+            </Tabs.Content>
+          </YStack>
+        </Tabs>
       </ScrollView>
       <NavBar />
     </>
